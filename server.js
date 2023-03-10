295f57dce72f2b44c2c534fe044be36da5ddff1a
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-03-09 22:16:46 -0700

    Deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
+ 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
